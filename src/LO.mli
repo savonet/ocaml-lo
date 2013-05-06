7d2ede5f4f346a965a3f26e6a7bc77c4db9d8487
Samuel Mimram <smimram@gmail.com>
2013-05-06 20:33:25 +0200

    Add some documentation.

--- src/LO.mli
+++ src/LO.mli
@@ -1,18 +1,24 @@
+(** Bindings to liblo library to send and receive OSC messages. *)
+
 exception Error
 exception Unhandled
 
+(** Operations on addresses. *)
 module Address : sig
+  (** An address. *)
   type t
 
+  (** Create an address with given host and port. *)
   val create : string -> int -> t
 end
 
+(** Messages. *)
 module Message : sig
   type timetag = int * int
 
   type data =
     [
-      `Int32 of int
+    | `Int32 of int
     | `Float of float
     | `String of string
     | `Blob of string
@@ -28,15 +34,23 @@ module Message : sig
     | `Infinitum
     ]
 
+  (** String representation of a message (useful for debugging). *)
   val to_string : data -> string
 end
 
+(** Send messages on given address, at given path. *)
 val send : Address.t -> string -> Message.data list -> unit
 
+(** Operations for creating servers. *)
 module Server : sig
+  (** A server. *)
   type t
 
+  (** Create a server listening on given port with handler function taking as
+      agument the path and messages received. *)
   val create : int -> (string -> Message.data array -> unit) -> t
 
+  (** Recive messages and pass them to the handler. This function is blocking
+      until a message is received. *)
   val recv : t -> unit
 end
